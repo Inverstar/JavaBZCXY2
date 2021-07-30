655554ef14246920dc7fdfd3af8a32fd739d95e0
verstar-zorin <lnq1208@163.com>
2021-07-31 01:00:06 +0800

    放弃ideaprojects, 使用自选仓库

---
+++ src/数据/标识符.java
@@ -0,0 +1,13 @@
+package 数据;
+
+/**
+ * 标识符用于给变量\类\方法\包进行命名
+ * @author verstar
+ * @version 1.0
+ */
+public class 标识符 {
+    /*标识符 字母 下划线 美元符号 开头 不能是关键字
+    *
+    *  */
+}
+// Java通常采用Unicode这样的国际字符集而不是ASCII字符集
---
+++ src/数据/注释.java
@@ -0,0 +1,20 @@
+package 数据;
+
+/**
+ * 我真的很想苦,能用了,还没好, 不跟屏,让我看看,还是没用阿
+ * 还是没用处不大
+ * 解决不了 看起來的
+ */
+public class 注释
+{
+    public static void main(String[] args /*参数名称, 可变*/)
+    {
+        System.out.println("这是一个注释类");
+    }
+}
+// 单行注释
+/**
+ * 注释类 第一个
+ * @author verstar
+ * @version 1.0
+ */
